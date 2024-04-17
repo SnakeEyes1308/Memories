b559586061799c202ae102e7b91fd2909d1681ef
SnakeEyes1308 <nisarg.patel.btech2021@sitpune.edu.in>
2024-04-17 15:43:05 +0530

    create a post api route

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'; 
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js'; 
 import cookieParser from 'cookie-parser';
+
 
 dotenv.config();
 
@@ -16,7 +18,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser()); 
+app.use(cookieParser());
+
 
 app.listen  (3000, () => {
   console.log('Server is running on port 3000 !');
@@ -24,6 +27,7 @@ app.listen  (3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
