bfaf565060d04e9330221f9881f28055403b61a8
SnakeEyes1308 <nisarg.patel.btech2021@sitpune.edu.in>
2024-04-27 14:28:08 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className=' min-h-screen flex items-center justify-center'>
+      <div className=' max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className=' text-3xl font-semibold text-center my-7'>About Memories Blog</h1>
+          <div className=' text-md text-gray-500 flex flex-col gap-6'>
+          <p>Memories Blog is a full-stack web application that allows users to create, read, update, and delete blog posts. The front-end is built with React and Tailwind CSS, and the back-end is built with Node.js, Express, and MongoDB. The application is deployed on Heroku and MongoDB Atlas.</p>
+          <p> Paragraph-2</p>
+          <p>Paragraph-3</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
